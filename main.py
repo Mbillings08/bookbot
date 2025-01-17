874b2a664032e3b9af0527bff8ee6a8d86f6a2c1
Mbillings08 <billings.michael.d@gmail.com>
2025-01-16 21:54:23 -0700

    starting to add letter counts

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def main():
             words = file_contents.split()
             wordcount = len(words)
             print(f"Total number of words: {wordcount}")
+
+            lower_file_contents = file_contents.lower()
+            lettercount = 0
+            letter_totals = []
+            for char in lower_file_contents:
+                if char.isalpha():
+                    lettercount += 1
+                    letter_totals.append(char)
+            print(lettercount)
+
     except FileNotFoundError:
         print("The file 'books/frankenstein.txt' was not found. Please check the path and try again." )
     except Exception as e:
