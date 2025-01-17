fd17a4dadc7694d92d2004ecae0bcd02daf12c78
Mbillings08 <billings.michael.d@gmail.com>
2025-01-16 21:41:36 -0700

    added text to output to make it more clear

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
             file_contents = f.read()
             words = file_contents.split()
             wordcount = len(words)
-            print(wordcount)
+            print(f"Total number of words: {wordcount}")
     except FileNotFoundError:
         print("The file 'books/frankenstein.txt' was not found. Please check the path and try again." )
     except Exception as e:
