6fea5218031b6032ee350833f88fd3b458fa5050
Mbillings08 <billings.michael.d@gmail.com>
2025-05-18 22:36:22 -0600

    added character count function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     
     from stats import get_num_words
-        
+    from stats import get_char_count
+    
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,36 @@ def get_num_words():
         print(f"An error occured: {e}")
 
 get_num_words()
+
+
+def get_char_count():
+    t = 0
+    p = 0
+    c = 0
+    characters = []
+    try:
+        with open("books/frankenstein.txt") as l:
+            for line in l:
+                for char in line:
+                    characters.append(char)
+        for letter in characters:
+            lower_letter = letter.lower()
+            if lower_letter == "t":
+                t += 1
+            if lower_letter == "p":
+                p += 1
+            if lower_letter == "c":
+                c += 1
+        #print(characters)
+        print(f"'t': {t}")
+        print(f"'p': {p}")
+        print(f"'c': {c}")
+                
+
+            
+
+    except FileNotFoundError:
+        print("The file 'books/frankenstein.txt' was not found. Please check the path and try again." )
+    except Exception as e:
+        print(f"An error occured: {e}")       
+get_char_count()
